757cb40f127db891fb72c2f736cab60cfc282dd5
HoonHee Jeong <jhh992000@gmail.com>
2021-04-30 04:59:57 +0900

    feat: 회차별 전진한 횟수를 반환하는 메서드 추가 및 테스트코드 추가

--- src/main/java/nextstep/study/racingcar/domain/CarSpeeds.java
+++ src/main/java/nextstep/study/racingcar/domain/CarSpeeds.java
@@ -7,10 +7,6 @@ public class CarSpeeds {
 
 	private List<Speed> speeds = new ArrayList<>();
 
-	public CarSpeeds() {
-
-	}
-
 	public void add(Speed speed) {
 		this.speeds.add(speed);
 	}
@@ -18,6 +14,18 @@ public class CarSpeeds {
 	public int getForwardCount() {
 		int sum = 0;
 		for (Speed speed : speeds) {
+			sum += speed.getForwardCount();
+		}
+		return sum;
+	}
+
+	public int getForwardCount(int playIndex) {
+		if (playIndex > speeds.size()) {
+			throw new IllegalArgumentException("경주 회차는 총 경주횟수보다 클 수 없습니다.");
+		}
+		int sum = 0;
+		for (int i = 0; i <= playIndex; i++) {
+			Speed speed = speeds.get(i);
 			sum += speed.getForwardCount();
 		}
 		return sum;
--- src/test/java/nextstep/study/racingcar/domain/CarSpeedsTest.java
+++ src/test/java/nextstep/study/racingcar/domain/CarSpeedsTest.java
@@ -19,4 +19,16 @@ public class CarSpeedsTest {
 		assertThat(carSpeeds.getForwardCount()).isEqualTo(2);
 	}
 
+	@Test
+	@DisplayName("회차별 경주횟수를 확인")
+	void test_forwardCountByPlayIndex() {
+		CarSpeeds carSpeeds = new CarSpeeds();
+		carSpeeds.add(new Speed(2));
+		carSpeeds.add(new Speed(3));
+		carSpeeds.add(new Speed(4)); //여기까지의 합
+		carSpeeds.add(new Speed(5));
+
+		assertThat(carSpeeds.getForwardCount(2)).isEqualTo(1);
+	}
+
 }
