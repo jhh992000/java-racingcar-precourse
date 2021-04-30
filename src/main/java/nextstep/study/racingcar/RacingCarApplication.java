9334a9d0755a52a3c012024d86a2dd9d0291a741
HoonHee Jeong <jhh992000@gmail.com>
2021-04-30 17:21:59 +0900

    refactor: 사용자에게 입력받는 부분을 분리함

--- src/main/java/nextstep/study/racingcar/RacingCarApplication.java
+++ src/main/java/nextstep/study/racingcar/RacingCarApplication.java
@@ -1,12 +1,23 @@
 package nextstep.study.racingcar;
 
 import nextstep.study.racingcar.controller.RaceController;
+import nextstep.study.racingcar.service.RaceInputService;
+
+import java.util.List;
 
 public class RacingCarApplication {
 
 	public static void main(String[] args) {
+
+		//자동차명과 시도횟수 입력 받기
+		RaceInputService inputService = new RaceInputService();
+		List<String> carNames = inputService.inputCarNames();
+		int raceCount = inputService.inputRaceCount();
+
+		//게임 시작하기
 		RaceController raceController = new RaceController();
-		raceController.startGame();
+		raceController.startGame(carNames, raceCount);
+
 	}
 
 }
--- src/main/java/nextstep/study/racingcar/controller/RaceController.java
+++ src/main/java/nextstep/study/racingcar/controller/RaceController.java
@@ -2,23 +2,23 @@ package nextstep.study.racingcar.controller;
 
 import nextstep.study.racingcar.domain.RaceGroup;
 import nextstep.study.racingcar.domain.RaceResult;
-import nextstep.study.racingcar.service.RaceInputService;
 import nextstep.study.racingcar.view.RaceView;
 
 import java.util.List;
 
 public class RaceController {
 
-	private final RaceInputService inputService = new RaceInputService();
+	public void startGame(List<String> carNames, int raceCount) {
 
-	public void startGame() {
-		List<String> carNames = inputService.inputCarNames();
-		int raceCount = inputService.inputRaceCount();
-
+		//경주 그룹 생성
 		RaceGroup raceGroup = new RaceGroup(carNames, raceCount);
+
+		//경주 시작
 		RaceResult raceResult = raceGroup.startRace();
 
+		//게임결과 출력하기
 		RaceView.printRaceResult(raceResult);
+
 	}
 
 }
